63881274c58354c64a898e3cd1e28c08a5995255
“mayluv17” <mayowaibrahim009@gmail.com>
2023-07-04 13:47:08 +0100

    header updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import { getServerSession } from "next-auth";
 import Link from "next/link";
-// import { ThemeToggle } from "./ThemeToggle";
+import { ThemeToggle } from "./ThemeToggle";
 import { buttonVariants } from "@/ui/Button";
 import SignInButton from "@/components/SignInButton";
 import SignOutButton from "@/components/SignOutButton";
@@ -16,10 +16,12 @@ const Navbar = async ({}) => {
           Text Similarity v1.0
         </Link>
 
-        <div className="md:hidden">{/* <ThemeToggle /> */}</div>
+        <div className="md:hidden">
+          <ThemeToggle />
+        </div>
 
         <div className="hidden md:flex gap-4">
-          {/* <ThemeToggle /> */}
+          <ThemeToggle />
           <Link
             href="/documentation"
             className={buttonVariants({ variant: "ghost" })}
