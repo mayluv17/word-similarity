1ebe0e249c932ad3e62803ea14e8013b2d06b944
“mayluv17” <mayowaibrahim009@gmail.com>
2023-09-29 18:44:58 +0100

    final commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,13 @@
-import { FC } from "react";
+import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { ThemeToggle } from "./ThemeToggle";
-import { buttonVariants } from "@/ui/Button";
-import SignInButton from "@/components/SignInButton";
-import SignOutButton from "@/components/SignOutButton";
+import { buttonVariants } from "./ui/Button";
+import SignInButton from "./ui/SignInButton";
+import SignOutButton from "./ui/SignOutButton";
 
-const Navbar = async ({}) => {
-  const session = await getServerSession();
+const Navbar = async () => {
+  const session = await getServerSession(authOptions);
 
   return (
     <div className="fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900/75 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between">
